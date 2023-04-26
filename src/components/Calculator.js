4c6ca27659daee39470dbc8012c0fee71c62c45b
yacoubou-seidou <yacoseidou@gmail.com>
2023-04-26 19:56:29 +0100

    Complete click event and calculator function implementation

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -9,7 +9,7 @@ const Calculator = () => {
     operation: null,
   });
 
-  const clickHandle = (e) => {
+  const eventHandler = (e) => {
     setValue((value) => ({
       ...value,
       ...calculate(value, e.target.name),
@@ -20,25 +20,25 @@ const Calculator = () => {
       <div className="calculator">
         <Monitor value={value} />
         <section className="calcbtnContainer">
-          <button onClick={clickHandle} type="button" name="AC">AC</button>
-          <button type="button" onClick={clickHandle} className="twosigne" name="+/-">-/+</button>
-          <button type="button" onClick={clickHandle} className="pourcent" name="%">%</button>
-          <button type="button" onClick={clickHandle} className="orange" name="÷">&#247;</button>
-          <button type="button" onClick={clickHandle} name="7">7</button>
-          <button type="button" onClick={clickHandle} name="8">8</button>
-          <button type="button" onClick={clickHandle} name="9">9</button>
-          <button type="button" className="orange" onClick={clickHandle} name="x">&#215;</button>
-          <button type="button" onClick={clickHandle} name="4">4</button>
-          <button type="button" onClick={clickHandle} name="5">5</button>
-          <button type="button" onClick={clickHandle} name="6">6</button>
-          <button type="button" className="orange" onClick={clickHandle} name="-">-</button>
-          <button type="button" onClick={clickHandle} name="1">1</button>
-          <button type="button" onClick={clickHandle} name="2">2</button>
-          <button type="button" onClick={clickHandle} name="3">3</button>
-          <button type="button" className="orange" onClick={clickHandle} name="+">+</button>
-          <button type="button" className="zero" onClick={clickHandle} name="0">0</button>
-          <button type="button" onClick={clickHandle} name=".">.</button>
-          <button type="button" className="orange" onClick={clickHandle} name="=">=</button>
+          <button onClick={eventHandler} type="button" name="AC">AC</button>
+          <button type="button" onClick={eventHandler} className="twosigne" name="+/-">-/+</button>
+          <button type="button" onClick={eventHandler} className="pourcent" name="%">%</button>
+          <button type="button" onClick={eventHandler} className="orange" name="÷">&#247;</button>
+          <button type="button" onClick={eventHandler} name="7">7</button>
+          <button type="button" onClick={eventHandler} name="8">8</button>
+          <button type="button" onClick={eventHandler} name="9">9</button>
+          <button type="button" className="orange" onClick={eventHandler} name="x">&#215;</button>
+          <button type="button" onClick={eventHandler} name="4">4</button>
+          <button type="button" onClick={eventHandler} name="5">5</button>
+          <button type="button" onClick={eventHandler} name="6">6</button>
+          <button type="button" className="orange" onClick={eventHandler} name="-">-</button>
+          <button type="button" onClick={eventHandler} name="1">1</button>
+          <button type="button" onClick={eventHandler} name="2">2</button>
+          <button type="button" onClick={eventHandler} name="3">3</button>
+          <button type="button" className="orange" onClick={eventHandler} name="+">+</button>
+          <button type="button" className="zero" onClick={eventHandler} name="0">0</button>
+          <button type="button" onClick={eventHandler} name=".">.</button>
+          <button type="button" className="orange" onClick={eventHandler} name="=">=</button>
         </section>
       </div>
     </>
