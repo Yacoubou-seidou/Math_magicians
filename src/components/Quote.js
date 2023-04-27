8aa76de2f1141c8e7e7d14d7e9c170276019ea4d
yacoubou-seidou <yacoseidou@gmail.com>
2023-04-27 04:14:31 +0100

    fetch data from api and display it

---
+++ src/components/Quote.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import axios from 'axios';
+
+const Quote = () => {
+  const [fetchData, setFetchData] = useState();
+  const [getError, setGetError] = useState();
+  const [loader, setLoader] = useState(true);
+  const getQuote = () => {
+    axios.get('https://api.api-ninjas.com/v1/quotes?category=happiness', {
+      headers: {
+        'X-Api-Key': '/Ngvmr+DmlIJq7t/dCmSPg==VGYgFDIj4NgtMCLz',
+        'Content-Type': 'application/json',
+      },
+    }).then((response) => {
+      setFetchData(response.data[0]);
+      setLoader(false);
+    }).catch((error) => {
+      setGetError(error.message);
+      setLoader(false);
+    });
+  };
+
+  useEffect(() => {
+    getQuote();
+  }, []);
+  return (
+    !loader ? (
+      <div className="quote">
+        {fetchData ? (
+          <>
+            <q className="paragraph">{fetchData.quote}</q>
+            <p className="author">
+              {fetchData.author}
+            </p>
+          </>
+        ) : (
+          <p className="paragraph">
+            {getError}
+          </p>
+        )}
+      </div>
+    ) : <p>Loading</p>
+  );
+};
+
+export default Quote;
